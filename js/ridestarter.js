876c6b9e9c5a84c9ae702976a5c4737770f44609
Ekkalux Nguyen <ekkalux_414@berkeley.edu>
2017-10-15 16:12:19 -0700

    ridestarter done

---
+++ js/ridestarter.js
@@ -0,0 +1,61 @@
+$( document ).ready(function() {
+
+  $(".fa-arrow-circle-o-down").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".navigation").offset().top},
+        'slow');
+});
+
+$("#summary").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".summary").offset().top},
+      'slow');
+});
+
+$("#solution").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".solution").offset().top},
+      'slow');
+});
+
+$("#research").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".research").offset().top},
+      'slow');
+});
+
+$("#design").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".design_choices").offset().top},
+      'slow');
+});
+
+$("#implementation").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".implementation").offset().top},
+      'slow');
+});
+
+$("#results").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".final").offset().top},
+      'slow');
+});
+
+$(function(){
+        // Check the initial Poistion of the Sticky Header
+        var stickyHeaderTop = $('.navigation').offset().top;
+
+        $(window).scroll(function(){
+                if( $(window).scrollTop() > stickyHeaderTop ) {
+                        $('.navigation').css({position: 'fixed', top: '0px'});
+                        $('#stickyalias').css('display', 'block');
+                } else {
+                        $('.navigation').css({position: 'static', top: '0px'});
+                        $('#stickyalias').css('display', 'none');
+                }
+        });
+  });
+
+
+});
