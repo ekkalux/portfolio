2880c301dfc858574f957516813633a6f38281a1
Ekkalux Nguyen <ekkalux_414@berkeley.edu>
2017-11-30 19:07:57 -0800

    gym added

---
+++ js/gym.js
@@ -0,0 +1,73 @@
+$( document ).ready(function() {
+
+  $(".fa-angle-double-down").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".navigation").offset().top},
+        'slow');
+});
+
+$("#summary").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".summary").offset().top},
+      'slow');
+});
+
+$("#solution").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".solution").offset().top},
+      'slow');
+});
+
+$("#research").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".research").offset().top},
+      'slow');
+});
+
+$("#design").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".design_choices").offset().top},
+      'slow');
+});
+
+$("#implementation").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".usability").offset().top},
+      'slow');
+});
+
+$("#visual").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".visual").offset().top},
+      'slow');
+});
+
+$("#conclusion").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".conclusion").offset().top},
+      'slow');
+});
+
+$(".fa-angle-double-up ").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".hero").offset().top},
+      'slow');
+});
+
+$(function(){
+        // Check the initial Poistion of the Sticky Header
+        var stickyHeaderTop = $('.navigation').offset().top;
+
+        $(window).scroll(function(){
+                if( $(window).scrollTop() > stickyHeaderTop ) {
+                        $('.navigation').css({position: 'fixed', top: '0px'});
+                        $('#stickyalias').css('display', 'block');
+                } else {
+                        $('.navigation').css({position: 'static', top: '0px'});
+                        $('#stickyalias').css('display', 'none');
+                }
+        });
+  });
+
+
+});
